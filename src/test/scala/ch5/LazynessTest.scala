f755710655026ea0236508ab75cd3cb8c17aa1d3
mt8u <5f35eab0-9058-4bd6-a2f6-a7a4b1904636@aleeas.com>
2024-04-03 18:32:01 +0200

    test: uncomment and fix Empty cases

--- src/test/scala/ch5/LazynessTest.scala
+++ src/test/scala/ch5/LazynessTest.scala
@@ -11,9 +11,9 @@ class ch5_Suite extends munit.FunSuite:
   test("take"):
     assertEquals(lazyList.take(2).toList, LazyList(1, 2).toList)
     assertEquals(lazyList.take(5).toList, LazyList(1, 2, 3).toList)
-    // assertEquals(LazyList.empty.toList, LazyList.empty.toList)
+    assertEquals(LazyList.Empty.take(2), LazyList.empty)
 
   test("drop"):
     assertEquals(lazyList.drop(2).toList, LazyList(3).toList)
     assertEquals(lazyList.drop(5).toList, LazyList.empty.toList)
-    // assertEquals(LazyList.empty.toList, LazyList.empty.toList)
+    assertEquals(LazyList.Empty.drop(2).toList, LazyList.empty.toList)
