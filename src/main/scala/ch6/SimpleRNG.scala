0c6dcaa50d4b794429e12098dd39aabc17d3f87e
mt8u <5f35eab0-9058-4bd6-a2f6-a7a4b1904636@aleeas.com>
2024-07-15 19:36:46 +0200

    exo. 6.3

--- src/main/scala/ch6/SimpleRNG.scala
+++ src/main/scala/ch6/SimpleRNG.scala
@@ -11,13 +11,38 @@ case class SimpleRNG(seed: Long) extends RNG:
     (n, nextRNG)
 
 object SimpleRNG:
-  def nonNegativeInt(rng: RNG): (Int, RNG) =
+  // Implémentation fausse : pas random car
+  // nonNegativeInt(Int.MinValue) == nonNegativeInt(Int.MinValue + 1)
+  def nonNegativeInt_fausse_implem(rng: RNG): (Int, RNG) =
     val (n, rng2) = rng.nextInt
     n match
       case Int.MinValue => (Int.MaxValue, rng2)
       case _: Int       => (Math.abs(n), rng2)
-      
-  def double(rng: RNG): (Double, RNG) =
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) =
+    val (i, r) = rng.nextInt
+    (if i < 0 then -(i + 1) else i, r)
+    
+  // Implémentation fausse car elle utilise la précédente
+  def double_fausse_implem(rng: RNG): (Double, RNG) =
     val (n, rng2) = nonNegativeInt(rng)
     ((-n.toDouble / Int.MinValue), rng2)
 
+  def double(rng: RNG): (Double, RNG) =
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+    
+  def intDouble(rng: RNG): ((Int, Double), RNG) = 
+    val (n, rng1) = rng.nextInt
+    val (d, rng2) = double(rng1)
+    ((n, d), rng2)
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) =
+    val ((n, d), rng1) = intDouble(rng)
+    ((d, n), rng1)
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) =
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1, d2, d3), rng3)
--- src/test/scala/ch6/SimpleRNGTest.scala
+++ src/test/scala/ch6/SimpleRNGTest.scala
@@ -4,22 +4,17 @@ class ch6_Suite extends munit.FunSuite:
 
   case class SimpleRNGMinInt(seed: Long) extends RNG:
     def nextInt: (Int, RNG) =
-      val newSeed = (seed * 0x5deece66dL + 0xbL) & 0xffffffffffffL
-      val nextRNG = SimpleRNG(newSeed)
-      (Int.MinValue, nextRNG)
+      (Int.MinValue, SimpleRNGMinInt(seed))
 
   case class SimpleRNGZero(seed: Long) extends RNG:
     def nextInt: (Int, RNG) =
-      val newSeed = 1// (seed * 0x5deece66dL + 0xbL) & 0xffffffffffffL
-      val nextRNG = SimpleRNG(newSeed)
-      (0, nextRNG)
+      (0, SimpleRNGZero(seed))
       
   test("nonNegativeInt"):
     val rng: RNG = SimpleRNG(42)
     val (n1, rng2) = rng.nextInt
     val (n2, rng3) = rng2.nextInt
-    assertEquals(n2, -1281479697)
-    assertEquals(SimpleRNG.nonNegativeInt(rng2)._1, 1281479697)
+    assertEquals(SimpleRNG.nonNegativeInt(rng2)._1, 1281479696)
     assertEquals(SimpleRNG.nonNegativeInt(SimpleRNGMinInt(42))._1, 2147483647)
 
   test("double"):
@@ -27,3 +22,33 @@ class ch6_Suite extends munit.FunSuite:
     assertEquals(SimpleRNG.double(SimpleRNGMinInt(0))._1, 0.9999999995343387)
     assertEquals(SimpleRNG.double(SimpleRNGZero(0))._1, 0.0)
     
+
+  test("intDouble"):
+    val rng: RNG = SimpleRNG(42)
+    assertEquals(
+      SimpleRNG.intDouble(SimpleRNGMinInt(0))._1, (-2147483648, 0.9999999995343387)
+    )
+    assertEquals(
+      SimpleRNG.intDouble(SimpleRNGZero(0))._1, (0, 0.0)
+    )
+
+  test("doubleInt"):
+    val rng: RNG = SimpleRNG(42)
+    assertEquals(
+      SimpleRNG.doubleInt(SimpleRNGMinInt(0))._1, (0.9999999995343387, -2147483648)
+    )
+    assertEquals(
+      SimpleRNG.doubleInt(SimpleRNGZero(0))._1, (0.0, 0)
+    )
+
+  test("double3"):
+    val rng: RNG = SimpleRNG(42)
+    assertEquals(
+      SimpleRNG.double3(SimpleRNGMinInt(0))._1,
+      (0.9999999995343387,
+        0.9999999995343387,
+        0.9999999995343387)
+    )
+    assertEquals(
+      SimpleRNG.double3(SimpleRNGZero(0))._1, (0.0, 0.0, 0.0)
+    )
