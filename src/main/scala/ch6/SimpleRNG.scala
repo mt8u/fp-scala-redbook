eeacf137ba2e0dfa90724561cc9661244e507862
mt8u <5f35eab0-9058-4bd6-a2f6-a7a4b1904636@aleeas.com>
2024-09-30 19:30:37 +0200

    state

--- src/main/scala/ch6/SimpleRNG.scala
+++ src/main/scala/ch6/SimpleRNG.scala
@@ -1,6 +1,18 @@
 package ch6
 
+opaque type State[S, +A] = S => (A, S)
+ 
+object State:
+  extension [S, A](underlying: State[S, A])
+    def run(s: S): (A, S) = underlying(s)
+ 
+  def unit[S, A](a: A): State[S, A] =
+    s => (a, s)
+
+  def apply[S, A](f: S => (A, S)): State[S, A] = f
+
 type Rand[+A] = RNG => (A, RNG)
+// type Rand[A] = State[RNG, A]
 
 trait RNG:
   def nextInt: (Int, RNG)
@@ -100,3 +112,4 @@ object SimpleRNG:
   def map2ViaFlatMap[A, B, C](rA: Rand[A], rB: Rand[B])(
       f: (A, B) => C
   ): Rand[C] = flatMap(rA)(a => map(rB)(f(a, _)))
+
