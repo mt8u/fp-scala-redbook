603ad498cb29338409179a902987353e6e1b64ad
JF NGBOKOLI <ngbokolifranck@yahoo.fr>
2024-11-04 19:36:58 +0100

    refactoring

--- src/main/scala/ch6/CandyMachine.scala
+++ src/main/scala/ch6/CandyMachine.scala
@@ -1,34 +1,35 @@
 package ch6
 
+import ch6.State.unit
+
 enum Input:
   case Coin, Turn
 
 type Candies = Int
 type Coins = Int
 
-case class Machine(locked: Boolean, candies: Candies, coins: Coins)
+case class Machine(locked: Boolean, candies: Candies, coins: Coins):
 
-def simulateMachine(inputs: List[Input]): State[Machine, (Coins, Candies)] =
-  State(m =>
-    inputs.foldRight(
-      ((m.coins, m.candies), m): ((Coins, Candies), Machine)
-    )((i, s) => {
-      i match
-        case Input.Coin => unlockMachine(s)
-        case Input.Turn => dispenseACandy(s)
-    })
-  )
+  def simulateMachine(inputs: List[Input]): State[Machine, (Coins, Candies)] =
+      inputs.foldRight(
+        unit((coins, candies)): State[Machine, (Coins, Candies)]
+      )((i, s) => {
+        i match
+          case Input.Coin => unlockMachine(s)
+          case Input.Turn => dispenseACandy(s)
+      })
 
 def unlockMachine(
-    state: ((Coins, Candies), Machine)
-): ((Coins, Candies), Machine) =
-  val ((coins, candies), mNext) = state
-  ((coins, candies), Machine(false, candies, coins))
+    state: State[Machine, (Coins, Candies)]
+): State[Machine, (Coins, Candies)] =
+  State(m => ((m.coins, m.candies), Machine(false, m.candies, m.coins)))
 
 def dispenseACandy(
-    state: ((Coins, Candies), Machine)
-): ((Coins, Candies), Machine) =
-  val ((coins, candies), mNext) = state
-  mNext.locked match
-    case false => ((coins + 1, candies - 1), Machine(false, candies, coins))
-    case true  => state
+    state: State[Machine, (Coins, Candies)]
+): State[Machine, (Coins, Candies)] =
+State(
+  m =>
+    m.locked match
+    case false => ((m.coins + 1, m.candies - 1), Machine(false, m.candies, m.coins))
+    case true  => ((m.coins, m.candies), m)
+)
--- src/test/scala/ch6/CandyMachineTest.scala
+++ src/test/scala/ch6/CandyMachineTest.scala
@@ -6,26 +6,26 @@ class simulate_candy_machine extends munit.FunSuite:
     "should returns O candy and zero coin when initial state has 0 candy and 0 coin"
   ):
     val machine = Machine(true, 0, 0)
-    val finalState = simulateMachine(List(Input.Coin)).run(machine)
+    val finalState = machine.simulateMachine(List(Input.Coin)).run(machine)
     assertEquals(finalState._1, (0, 0))
 
   test(
     "should returns 1 candy and 0 coin when initial state has 1 candy and 0 coin and no coin is inserted"
   ):
     val machine = Machine(true, 1, 0)
-    val finalState = simulateMachine(List()).run(machine)
+    val finalState = machine.simulateMachine(List()).run(machine)
     assertEquals(finalState._1, (0, 1))
 
   test(
     "should unlocks the machine when initial state is locked with 1 candy and 0 coin, and a coin is inserted"
   ):
     val machine = Machine(true, 1, 0)
-    val finalState = simulateMachine(List(Input.Coin)).run(machine)
+    val finalState = machine.simulateMachine(List(Input.Coin)).run(machine)
     assertEquals(finalState, ((0, 1), Machine(false, 1, 0)))
 
   test(
     "should dispense a candy when the machine is unlocked with 1 candy and 0 coin, and the knob is turned"
   ):
     val machine = Machine(false, 1, 0)
-    val finalState = simulateMachine(List(Input.Turn)).run(machine)
+    val finalState = machine.simulateMachine(List(Input.Turn)).run(machine)
     assertEquals(finalState._1, (1, 0))
