580130ed573689c5e35631d098c2df3b53f318bb
mt8u <5f35eab0-9058-4bd6-a2f6-a7a4b1904636@aleeas.com>
2024-12-16 20:41:54 +0100

    exo 7.3 (hard)

---
+++ src/main/scala/ch7/Par.scala
@@ -0,0 +1,74 @@
+package ch7
+
+import java.util.concurrent.TimeUnit
+
+def sum(ints: IndexedSeq[Int]): Int =
+  if ints.size <= 1 then ints.headOption.getOrElse(0)
+  else
+    val (l, r) = ints.splitAt(ints.size / 2)
+    sum(l) + sum(r)
+    
+trait ExecutorService:
+  def submit[A](a: Callable[A]): Future[A]
+ 
+trait Callable[A]:
+  def call: A
+ 
+trait Future[A]:
+  def get: A
+  def get(timeout: Long, unit: TimeUnit): A
+  def cancel(evenIfRunning: Boolean): Boolean
+  def isDone: Boolean
+  def isCancelled: Boolean
+  
+opaque type Par[A] = ExecutorService => Future[A]
+
+extension [A](pa: Par[A]) def run(s: ExecutorService): Future[A] = pa(s)
+
+object Par:
+  def unit[A](a: A): Par[A] = es => UnitFuture(a)
+ 
+  private case class UnitFuture[A](get: A) extends Future[A]:
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+    
+  private case class MapFuture[A, B, C](fa: Future[A], fb: Future[B], f: (A, B) => C) extends Future[C]:
+    var done = false
+    var cancelled = false
+    
+    def isDone = done
+
+    def get: C =
+      val c = f(fa.get, fb.get)
+      done = true
+      return c
+    
+    def get(timeout: Long, units: TimeUnit): C =
+      val timeoutNanos = units.toNanos(timeout)
+      val start = System.nanoTime()
+      val a = fa.get(timeout, units)
+      val end = System.nanoTime()
+      val duration = end - start
+      val b = fb.get(timeout - duration, TimeUnit.NANOSECONDS)
+      done = true
+      return f(a, b)
+    
+    def isCancelled = cancelled
+    
+    def cancel(evenIfRunning: Boolean): Boolean =
+      cancelled = true
+      return true
+ 
+  extension [A](pa: Par[A])
+    def map2[B, C](pb: Par[B])(f: (A, B) => C): Par[C] = 
+      (es: ExecutorService) => 
+        val futureA = pa(es)
+        val futureB = pb(es)
+        MapFuture(futureA, futureB, f)
+ 
+  def fork[A](a: => Par[A]): Par[A] = 
+    es => es.submit(new Callable[A] {
+      def call = a(es).get
+    })
